b2fe52fe15c74d85ffd9d9590321998f9b75910c
njoosse <njoosse@hotmail.com>
2017-08-10 08:57:30 -0400

    Deleted empty/unused files

--- spec/cpp-compiler-view-spec.js
+++
@@ -1,9 +0,0 @@
-'use babel';
-
-import CppCompilerView from '../lib/cpp-compiler-view';
-
-describe('CppCompilerView', () => {
-  it('has one valid test', () => {
-    expect('easy').toBe('easy');
-  });
-});
